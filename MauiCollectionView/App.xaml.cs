4e0ce76ee9b727f675b46ed162e1c56ad8a286b8
Alexandre Nunes <alexandrenunesmartins@hotmail.com>
2025-04-13 10:21:32 -0300

    Navigation Page 1 com botão

--- MauiCollectionView/App.xaml.cs
+++ MauiCollectionView/App.xaml.cs
@@ -8,7 +8,8 @@ namespace MauiCollectionView
         {
             InitializeComponent();
 
-            MainPage = new ContatosView();
+            //MainPage = new ContatosView();
+            MainPage = new NavigationPage(new MainPage());
         }
     }
 }
